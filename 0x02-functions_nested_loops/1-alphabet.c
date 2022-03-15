c164964f858852ea86fc69001cacbeeddec7a343
Ernielyn <ernesttwinomuhwezi@gmail.com>
2022-03-15 15:33:32 -0700

    Script to print the alphabet and a new line

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+*print_alphabet - a function which prints the alphabet in lower case
+*
+*Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+	char ch = a;
+
+	while(ch<=z)
+	{
+		_putchar(ch);
+		c++;
+	}
+	_putchar('\n');
+}
