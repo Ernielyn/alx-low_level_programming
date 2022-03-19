c40ccede376f2177d997c3342a9badb057db92be
Ernielyn <ernesttwinomuhwezi@gmail.com>
2022-03-18 19:21:56 -0700

    Correct syntax for the function to print out numbers 0-9, excluding 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,13 +1,18 @@
 #include "main.h"
 /**
- * print_most_numbers - a function to print the numbers from 9 to 10, followed by a new line buut not 2 and 4
+ * print_most_numbers - a function to print the numbers from 0-9 but not 2 and 4
+ *
+ * Return: a list of numbers from 0-9 excluding 2 & 4
  */
 void print_most_numbers(void)
 {
 	int j;
 
-	for (j = 0; j <= 9 && j != 2 && j != 4; j++)
+	for (j = 0; j <= 9; j++)
 	{
+		if((j == 2) || (i == 4))
+			continue;
+		else
 		_putchar(j + '0');
 	}
 	_putchar('\n');
