b78413baa993c2b2d1ba79c1ee38c360d01b6709
Ernielyn <ernesttwinomuhwezi@gmail.com>
2022-03-19 13:30:11 -0700

    Change the puts syntax in the program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,15 +3,12 @@
 /**
 *Main - serves as the starting point for program execution
 *
-*the function returns an integer value
-*
-*return: always 0 Success
+*Return: always 0 Success
 */
+
 int main(void)
 {
-	char str1[] = "Programming is like building a multilingual puzzle";
-
-	puts(str1);
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
