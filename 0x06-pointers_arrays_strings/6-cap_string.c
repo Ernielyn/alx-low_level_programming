99c82400e6d50c42bcd3fb0b9aecee4c74a8bc4b
Ernielyn <ernesttwinomuhwezi@gmail.com>
2022-03-25 14:37:16 -0700

    Add a function to check for the separators

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,44 +2,48 @@
 #include <string.h>
 #include <ctype.h>
 
+int search_separators(char c);
+
 /**
- * *cap_string - a function to capitalize all words of a string
+ * search_separators - a function to check for separators
  *
- * @s: a pointer to the string
+ * @c: input variable of character type
+ *
+ * Return: 1 if c is a character
+ */
+
+int search_separators(char c)
+{
+	int i;
+	char separators[15] = {'\t', '\n', ' ', ',', ':', '.', '!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; i  <= 15; i++)
+	{
+		if (c == separators[i])
+			return (1);
+		else
+			return (0);
+	}
+}
+
+/**
+ * *cap_string(char *s);
+ *
+ * @s: input pointer to the string
  *
  * Return: a string with capitalized words
  */
 
-char *cap_string(char *s)
+char *cap_string(char *);
 {
-	int i, k, n;
-	char separators[15] = {'\t', '\n', ' ', ',', ':', '.', '!', '?', '"', '(', ')', '{', '}'};
-	char c;
-
-	n = strlen(s);
-
-	for (i = 0; i  <= n; i++)
+	for (i =0; s[i] != '\0'; i++)
 	{
-		if  (i == 0)
-		{
-			if (s[i] >= 'a' && s[i] <= 'z')
+		if (i == 0  && s[i] >= 'a' && s[i] <= 'z')
 				s[i] = s[i] - 32;
-			continue;
-		}
 
-		for (j = 0; j < 15, j++)
-		{
-			if (s[i] == separators[j];
-				k = 1;
-			continue;
-		}
-				
-		if (k == 1 && s[i] >= 'a' && s[i] <= 'z')
-			s[i + 1] = s[i] - 32;
-		
-		else
-			s[i] = s[i];
+		if (search_separators(s[i]) && s[i + 1] >= 'a' && s[i + 1] <= 'z')
+			s[i + 1] = s[i + 1] - 32;
 	}
-
-		return (s);
+	
+	return (s);
 }
